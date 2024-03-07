1b7d9eef5e315676fc78333151110b281869b841
Roger <94903084+rogerdapalma@users.noreply.github.com>
2024-03-07 19:36:34 -0300

    Update ContaCaracter.js

--- Praticas/ContaCaracter.js
+++ Praticas/ContaCaracter.js
@@ -3,13 +3,13 @@ var numeroDeCaracteres = mensagem.length;
 console.log(numeroDeCaracteres);
 
 /*
- variavel 16 bits = 32 bits + TAM 80 bits
+ variavel 16 bits = 32 bits + TAM 1 bits
  instrução 16 bits = 48 bits
  Total bits = 160 bits
  ___________________________________________
 | Variável            | Tipo               |
 |---------------------|--------------------|
-| mensagem            | string   +  TAM    |
+| mensagem            | string   +  String |
 | numeroDeCaracteres  | const              |             
 | Instrução           | atribuição         |
 |                     | chamada de método  |
